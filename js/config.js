03bf0251c2b75473b9bec4c264d3e27195bb482f
AKSHAY MEHTA <136840035+AKSHAYKM0077@users.noreply.github.com>
2025-04-05 15:14:57 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_kDBsYio8Y', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5o8vo2fb6st6e21hkknldvcesd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
